807e26df241cc309310a93948cac1059984c91b7
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-12-19 23:12:45 +0000

    Added some logic for the main todolist application

---
+++ src/components/ToDoList.js
@@ -0,0 +1,53 @@
+
+class ToDoList {
+    constructor(title, description, dueDate, priority, notes) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+    }
+
+    changePriority(newPriority) {
+        this.priority = newPriority;
+    }
+
+    changeTitle(newTitle) {
+        this.title = newTitle;
+    }
+
+    changeDescription(newDescription) {
+        this.description = newDescription;
+    }
+
+    changeDueDate(newDueDate) {
+        this.dueDate = newDueDate;
+    }
+
+    changeNotes(newNotes) {
+        this.notes = newNotes;
+    }
+
+    getPriority() {
+        return this.priority;
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    getDescription() {
+        return this.description;
+    }
+
+    getDueDate() {
+        return this.dueDate;
+    }
+
+    getNotes() {
+        return this.notes;
+    }
+
+    
+    
+}
